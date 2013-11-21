79d24fa322ce6c30f94a871f6e9c3999b9304383
alessandro alessio <a.alessio86@gmail.com>
2013-11-21 22:15:17 +0100

    Create compress.php and update class.compress.php

--- class.compress.php
+++ class.compress.php
@@ -7,15 +7,46 @@ class Compress{
 	
 	public $FileURL;
 	public $FileContent;
+	public $FileContentType;
 
 	function __construct( $FileURL ) {
 		$this->FileURL = $FileURL;
+		$this->defineType( $this->FileURL );
+		$this->openFile( $this->FileURL );
+		$this->cleanFile( $this->FileContent );
+		$this->outputFile();
 	}
 
-	public function openFile() {
-		$FileHandle = fopen( $this->FileURL, "r" );
-		$FileContent = stream_get_contents( $FileHandle );
+	public function defineType( $File ) {
+		$Ext = pathinfo( $File, PATHINFO_EXTENSION );
+		switch ( $Ext ) {
+			case "css":
+				$this->FileContentType = "Content-type: text/css";
+			break;
+ 
+			case "js":
+				$this->FileContentType = "Content-type: text/javascript";
+			break;
+		}
+	}
+
+	public function openFile( $File ) {
+		$FileHandle = fopen( $File, "r" );
+		$this->FileContent = stream_get_contents( $FileHandle );
 		fclose( $FileHandle );
+	}
+
+	public function cleanFile( $Content ) {
+		$Search = array( "  ", "\n", "\r", "\t" );
+		$Replace = array( "" );
+		$Content = str_replace( $Search, $Replace, $Content );
+		$this->FileContentType = $Content;
+	}
+
+	public function outputFile() {
+		header( "Content-type: ".$this->FileContentType );
+		header( "Cache-control: must-revalidate" );
+		echo $this->FileContentType;
 	}
 
 }
---
+++ compress.php
@@ -0,0 +1,11 @@
+<?php
+
+// Require Class
+require("class.compress.php");
+
+// Setup Class
+$compress = New Compress( $_GET["fileurl"] );
+
+
+
+?>
