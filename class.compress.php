09ccd78b6e730e84594feb333db1bd66954e13be
Alessandro Alessio <info@a2area.it>
2013-11-21 17:20:43 +0100

    Update class.compress.php

--- class.compress.php
+++ class.compress.php
@@ -1,3 +1,23 @@
 <?php
 
+/**
+* class.compress.php
+*/
+class Compress{
+	
+	public $FileURL;
+	public $FileContent;
+
+	function __construct( $FileURL ) {
+		$this->FileURL = $FileURL;
+	}
+
+	public function openFile() {
+		$FileHandle = fopen( $this->FileURL, "r" );
+		$FileContent = stream_get_contents( $FileHandle );
+		fclose( $FileHandle );
+	}
+
+}
+
 ?>
